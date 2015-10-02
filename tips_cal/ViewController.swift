063bae77f7addbddf301b86134468341058a0e75
Nick Del Grande <szhang5@l-sb911kyg8w-m.homeoffice.wal-mart.com>
2015-10-02 13:14:18 -0700

    Initial Commit

---
+++ tips_cal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips_cal
+//
+//  Created by Shunan Zhang on 10/2/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
